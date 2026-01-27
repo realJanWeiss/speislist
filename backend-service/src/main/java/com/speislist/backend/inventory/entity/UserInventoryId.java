a254727ec2c5944f2139b2b2ab45a703376b53aa
realJanWeiss <171038268+realJanWeiss@users.noreply.github.com>
2026-01-27 12:27:08 +0100

    use lombok to remove code

--- backend-service/src/main/java/com/speislist/backend/inventory/entity/UserInventoryId.java
+++ backend-service/src/main/java/com/speislist/backend/inventory/entity/UserInventoryId.java
@@ -2,32 +2,18 @@ package com.speislist.backend.inventory.entity;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.Objects;
 
 @Embeddable
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class UserInventoryId {
     private String userId;
     private Long inventoryId;
-
-    // TODO check if they can be replaced with Lombok @EqualsAndHashCode
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserInventoryId that = (UserInventoryId) o;
-        return Objects.equals(userId, that.userId) && Objects.equals(inventoryId, that.inventoryId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(userId, inventoryId);
-    }
 }
--- backend-service/src/main/java/com/speislist/backend/shoppinglist/entity/UserShoppingListId.java
+++ backend-service/src/main/java/com/speislist/backend/shoppinglist/entity/UserShoppingListId.java
@@ -2,33 +2,20 @@ package com.speislist.backend.shoppinglist.entity;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 @Embeddable
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class UserShoppingListId implements Serializable {
     private String userId;
     private Long shoppingListId;
-
-    // TODO check if they can be replaced with Lombok @EqualsAndHashCode
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserShoppingListId that = (UserShoppingListId) o;
-        return Objects.equals(userId, that.userId) && Objects.equals(shoppingListId, that.shoppingListId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(userId, shoppingListId);
-    }
 }
